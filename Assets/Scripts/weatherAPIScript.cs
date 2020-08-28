04975fbb11cf9f463d38eca481e68a054d5a698b
Anthony Desktop <aventu6@uic.edu>
2020-08-28 16:25:11 -0500

    Completed Week 2 intro

---
+++ Assets/Scripts/weatherAPIScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+using TMPro;
+
+public class weatherAPIScript : MonoBehaviour
+{
+    public GameObject weatherTextObject;
+    string url = "http://api.openweathermap.org/data/2.5/weather?lat=41.88&lon=-87.6&APPID=84d195e355ae1f1edaa2ae3273a6937e&units=imperial";
+
+
+    void Start() {
+
+        // wait a couple seconds to start and then refresh every 900 seconds
+
+        InvokeRepeating("GetDataFromWeb", 2f, 900f);
+    }
+
+    void GetDataFromWeb() {
+
+        StartCoroutine(GetRequest(url));
+    }
+
+    IEnumerator GetRequest(string uri) {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+
+            if (webRequest.isNetworkError) {
+                Debug.Log(": Error: " + webRequest.error);
+            } else {
+                // print out the weather data to make sure it makes sense
+                Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
