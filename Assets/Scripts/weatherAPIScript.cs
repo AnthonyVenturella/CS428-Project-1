d2b161892283415428d66e0f439213297a222305
Anthony Desktop <aventu6@uic.edu>
2020-09-17 00:52:07 -0500

    added wind sock temp and humidity full functionality
    all remaining is conditions

--- Assets/Scripts/weatherAPIScript.cs
+++ Assets/Scripts/weatherAPIScript.cs
@@ -55,10 +55,20 @@ public class weatherAPIScript : MonoBehaviour{
     public GameObject windDirTextObject;
     public GameObject windSpeedTextObject;
 
+	public GameObject windSock;
+	public GameObject windSockParent;
+
+	public GameObject[] temperatureDiscs;
+	public GameObject[] humidityDisks;
+
+	private int _tempDiscCount;
+	private int _humidityDiskCount;
+
 	private float _temperature;
 	private float _humidity;
 	private float _windSpeed;
 	private float _windDir;
+	private float _mappedSpeed;
 
 	private string _description;
 	private string _cardinalDir;
@@ -92,14 +102,16 @@ public class weatherAPIScript : MonoBehaviour{
             } else {
                 // print out the weather data to make sure it makes sense
                 string data = webRequest.downloadHandler.text;
-                //Debug.Log(":\nReceived: " + data);
+
+				//used in temperature and humidity widget for the inside "liquid"
+				unAssignDisks();
 
 				/*May future generations forgive me for this abomination ive created. Mono doesnt have support for JSON parsing natively
 				  but has support for XML(??), I dont know anything about XML and I dont want to use a third party library like MiniJSON.cs
 				  (https://gist.github.com/darktable/1411710) since its a school project. Soooooo we're going to parse this JSON input 
 				  using C# string manipulation (i'm so sorry)*/
 
-				//Splitting input into new array elements after ever comma and bracket. Will make a lot of garbage but at least our data
+				//Splitting input into new array elements after every comma and bracket. Will make a lot of garbage but at least our data
 				//	will be in splittable tupple and not a massive string
 				string[] subStrings = data.Split(new char[] { ',', '{', '}' });
 
@@ -110,90 +122,29 @@ public class weatherAPIScript : MonoBehaviour{
                     //the true monstrosity begins here....
                     if (str.StartsWith("\"temp\":")) {
 						_temperature = float.Parse(str.Substring(7));
-						//Debug.Log("Temperature: " + _temperature);
 
 					} else if (str.StartsWith("\"humidity\":")) {
 						_humidity = float.Parse(str.Substring(11));
-						//Debug.Log("Humidity: " + _humidity);
 
 					} else if (str.StartsWith("\"description\":")) {
 						_description = str.Substring(14);
-						//Debug.Log("Description: " + _description);
 
 					} else if (str.StartsWith("\"speed\":")) {
 						_windSpeed = float.Parse(str.Substring(8));
-						//Debug.Log("Speed: " + _windSpeed);
 
 					} else if (str.StartsWith("\"deg\":")) {
 						_windDir = float.Parse(str.Substring(6));
-						//Debug.Log("Direction: " + _windDir);
 					}
 				}
 
-				/*	Assuming wndDir is based off of true north azimuths
-						N   =     0 deg		S   =   180 deg
-						NNE =  22.5 deg		SSW = 202.5 deg
-						NE  =    45 deg		SW  =   225 deg
-						ENE =  67.5 deg		WSW = 247.5 deg
-						E   =    90 deg		W	=   270 deg
-						ESE = 112.5 deg		WNW = 292.5 deg
-						SE  =   135 deg		NW	=	315 deg
-						SSE = 157.5 deg		NNW = 337.5 deg
+				_cardinalDir = getWindCardinalDir(_windDir);
 
-						Zones are 11.25 deg to either side of prime IE N is 11.25 - 348.75
-				 */
+				assignDisks(getPercentRange(_temperature), temperatureDiscs);
+				assignDisks(getPercentRange(_humidity), humidityDisks);
 
-				//continuing the "wtf man" code
-				if((_windDir >= 0 && _windDir < 11.25) || (_windDir >= 348.75 && _windDir < 360)) {
-					_cardinalDir = "N";
-
-                } else if (_windDir >= 11.25 && _windDir < 33.75) {
-					_cardinalDir = "NNE";
-
-				} else if (_windDir >= 33.75 && _windDir < 56.25) {
-					_cardinalDir = "NE";
-
-				} else if (_windDir >= 56.25 && _windDir < 78.75) {
-					_cardinalDir = "ENE";
-
-				} else if (_windDir >= 78.75 && _windDir < 101.25) {
-					_cardinalDir = "E";
-
-				} else if (_windDir >= 101.25 && _windDir < 123.75) {
-					_cardinalDir = "ESE";
-
-				} else if (_windDir >= 123.75 && _windDir < 146.25) {
-					_cardinalDir = "SE";
-
-				} else if (_windDir >= 146.25 && _windDir < 168.75) {
-					_cardinalDir = "SSE";
-
-
-				} else if (_windDir >= 168.75 && _windDir < 191.25) {
-					_cardinalDir = "S";
-
-				} else if (_windDir >= 191.25 && _windDir < 213.75) {
-					_cardinalDir = "SSW";
-
-				} else if (_windDir >= 213.75 && _windDir < 236.25) {
-					_cardinalDir = "SW";
-
-				} else if (_windDir >= 236.25 && _windDir < 258.75) {
-					_cardinalDir = "WSW";
-
-				} else if (_windDir >= 258.75 && _windDir < 281.25) {
-					_cardinalDir = "W";
-
-				} else if (_windDir >= 281.25 && _windDir < 303.75) {
-					_cardinalDir = "WNW";
-
-				} else if (_windDir >= 303.75 && _windDir < 326.25) {
-					_cardinalDir = "NW";
-
-				} else if (_windDir >= 326.25 && _windDir < 348.75) {
-					_cardinalDir = "NNW";
-
-				}
+				_mappedSpeed = mapWindSpeedRange(_windSpeed);
+				windSock.transform.localScale = new Vector3(1.0f, _mappedSpeed, 1.0f);
+				windSockParent.transform.Rotate(1.0f, _windDir, 1.0f, Space.Self);
 
 				temperatureTextObject.GetComponent<TextMeshPro>().text = _temperature + " \u00B0F";
 				humidityTextObject.GetComponent<TextMeshPro>().text = _humidity + "% Humidity";
@@ -204,5 +155,115 @@ public class weatherAPIScript : MonoBehaviour{
 
 		}
     }
+
+	private int getPercentRange(float value) {
+		if (value > 100) {
+			return 10;
+		} else if (value > 90 && value <= 100) {
+			return 9;
+		} else if (value > 80 && value <= 90) {
+			return 8;
+		} else if (value > 70 && value <= 80) {
+			return 7;
+		} else if (value > 60 && value <= 70) {
+			return 6;
+		} else if (value > 50 && value <= 60) {
+			return 5;
+		} else if (value > 40 && value <= 50) {
+			return 4;
+		} else if (value > 30 && value <= 40) {
+			return 3;
+		} else if (value > 20 && value <= 30) {
+			return 2;
+		} else if (value > 10 && value <= 20) {
+			return 1;
+		} else {
+			return 0;
+		}
+    }
+
+	private void assignDisks(int disks, GameObject[] diskArray) {
+		for(int i = 0; i < disks; i++) {
+			diskArray[i].gameObject.SetActive(true);
+		}
+	}
+
+	private void unAssignDisks() {
+		for (int i = 0; i < 10; i++) {
+			temperatureDiscs[i].gameObject.SetActive(false);
+			humidityDisks[i].gameObject.SetActive(false);
+		}
+	}
+
+
+	/*	Assuming wndDir is based off of true north azimuths
+			N   =     0 deg		S   =   180 deg
+			NNE =  22.5 deg		SSW = 202.5 deg
+			NE  =    45 deg		SW  =   225 deg
+			ENE =  67.5 deg		WSW = 247.5 deg
+			E   =    90 deg		W	=   270 deg
+			ESE = 112.5 deg		WNW = 292.5 deg
+			SE  =   135 deg		NW	=	315 deg
+			SSE = 157.5 deg		NNW = 337.5 deg
+
+			Zones are 11.25 deg to either side of prime IE N is 11.25 - 348.75
+	 */
+	private string getWindCardinalDir(float dir) {
+		if ((dir >= 0 && dir < 11.25) || (dir >= 348.75 && dir < 360)) {
+			return "N";
+
+		} else if (dir >= 11.25 && dir < 33.75) {
+			return "NNE";
+
+		} else if (dir >= 33.75 && dir < 56.25) {
+			return "NE";
+
+		} else if (dir >= 56.25 && dir < 78.75) {
+			return "ENE";
+
+		} else if (dir >= 78.75 && dir < 101.25) {
+			return "E";
+
+		} else if (dir >= 101.25 && dir < 123.75) {
+			return "ESE";
+
+		} else if (dir >= 123.75 && dir < 146.25) {
+			return "SE";
+
+		} else if (dir >= 146.25 && dir < 168.75) {
+			return "SSE";
+
+		} else if (dir >= 168.75 && dir < 191.25) {
+			return "S";
+
+		} else if (dir >= 191.25 && dir < 213.75) {
+			return "SSW";
+
+		} else if (dir >= 213.75 && dir < 236.25) {
+			return "SW";
+
+		} else if (dir >= 236.25 && dir < 258.75) {
+			return "WSW";
+
+		} else if (dir >= 258.75 && dir < 281.25) {
+			return "W";
+
+		} else if (dir >= 281.25 && dir < 303.75) {
+			return "WNW";
+
+		} else if (dir >= 303.75 && dir < 326.25) {
+			return "NW";
+
+		} else if (dir >= 326.25 && dir < 348.75) {
+			return "NNW";
+		} else {
+			return "NaN";
+        }
+	}
+
+	private float mapWindSpeedRange(float _speed) {
+
+		return _speed * 1.0f / 50;
+	}
 
 }
