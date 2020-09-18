208eedfd2f2df5d19682ad38a00b1259417cbf11
Anthony Desktop <aventu6@uic.edu>
2020-09-18 01:27:16 -0500

    added thunder sound to thunderstorm condition
    http://soundbible.com/1913-Thunder-HD.html

---
+++ Assets/Scripts/soundParticle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class soundParticle : MonoBehaviour {
+    public AudioClip spawnSound;
+    
+    private ParticleSystem _particleSystem;
+    private int _currentCount = 0;
+
+    private void Start() {
+        _particleSystem = this.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if(_particleSystem.particleCount > _currentCount) {
+            AudioSource.PlayClipAtPoint(spawnSound, new Vector3(5, 1, 2));
+        }
+
+        _currentCount = _particleSystem.particleCount;
+    }
+}
